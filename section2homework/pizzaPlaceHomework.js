bc23ee851ebb53539455aefae86d70f2f9f51360
Alexander Gochnour <alexandergochnour@gmail.com>
2022-06-17 17:33:52 -0500

    Added functionality to print statements in section2homework

--- section2homework/pizzaPlaceHomework.js
+++ section2homework/pizzaPlaceHomework.js
@@ -3,10 +3,10 @@
 const pizzaPlace = "Alexander's Pizza Place";
 let numberOfToppings = 12;
 // Print the variables and their types.
-console.log(typeof pizzaPlace);
-console.log(typeof numberOfToppings);
+console.log(pizzaPlace,typeof pizzaPlace);
+console.log(numberOfToppings,typeof numberOfToppings);
 // Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
-console.log(`"Here at ${pizzaPlace} we offer ${numberOfToppings} toppings to choose from."`);
+console.log(`Here at ${pizzaPlace} we offer ${numberOfToppings} toppings to choose from.`);
 /* Construct an if statement that prints "Quality, not quantity."
 if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings. */
 if (numberOfToppings < 10) {
