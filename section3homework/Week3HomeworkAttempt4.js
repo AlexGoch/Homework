90108e707f6bccebf8caa652be6aba26a2b3c7fe
Alexander Gochnour <alexandergochnour@gmail.com>
2022-06-26 16:21:02 -0500

    updates

--- section3homework/Week3HomeworkAttempt4.js
+++ section3homework/Week3HomeworkAttempt4.js
@@ -4,17 +4,13 @@ const pizzaToppings = ['Mushroom', 'Spinach', 'Feta', 'Onion', 'Tomato'];
 
 // 2. Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings
 
-
 function greetCustomer() {
-  let toppings = "";
+  let toppings = '';
   for (let element of pizzaToppings) {
-    toppings += element + " "
+    toppings += element + ' '
   };
   return console.log(`Welcome to Alexander's Pizza! Our toppings today are: ${toppings}`);
 };
-
-
-
 
 /* 3. Create a getPizzaOrder function that:
 -Has the parameters: size, crust, and in indefinite amount of toppings as inputs
@@ -23,7 +19,7 @@ function greetCustomer() {
 
 function getPizzaOrder(size,crust,...toppings) {
   let pizzaArr =[size,crust,toppings];
-  console.log(`One ${pizzaArr[0], pizzaArr[1]} crust pizza with ${pizzaArr[2]} coming up!`);
+  console.log(`One ${pizzaArr[0]} ${pizzaArr[1]} crust pizza with ${pizzaArr[2]} coming up!`);
   return pizzaArr;
   }
 
@@ -32,7 +28,6 @@ function getPizzaOrder(size,crust,...toppings) {
 -prints something like "...cooking pizza..."
 -outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings */
 
-// arrParam in this function is used to pull the returned array from the getPizzaOrder function.
 
 function preparePizza(arrParam){
   let pizzaObj = {
@@ -49,25 +44,17 @@ function preparePizza(arrParam){
 -logs a message that the pizza is ready and repeats the order, i.e. "Order up! Here's your large thick crust pizza with x,y,z, ...enjoy!"
 -outputs the same pizza Object that was passed in */
 
-// objParam in this function is used to pull the returned object from the preparePizza function.
 
 function servePizza(objParam) {
-  let pizzaObj = objParam;
   console.log(`Order up! Here's your ${objParam.size} ${objParam.crust} crust pizza with ${objParam.toppings}...enjoy!`);
-  return pizzaObj;
+  return objParam;
 }
 
 /* 6. Call each function and (starting with preparePizza) use the returned value from the previous function as its input.
 No hard-coding necessary in this part. */
 
-
 greetCustomer();
-servePizza(preparePizza(getPizzaOrder('large','thin','Mushroom','Spinach','Feta','Onion','Tomato')));
-
-
-
-
-
+servePizza(preparePizza(getPizzaOrder('large','thick','Mushroom','Spinach','Feta','Onion','Tomato')));
 
 // bonus answers below are work-in-progress and don't function correctly
 
@@ -83,18 +70,17 @@ return toppingsArr
 }
 listToppings(pizzaToppings);
 
-
 /* 10. In getPizzaOrder, if an order is placed with no toppings, print a slightly different message, like "One large thick crust cheese pizza coming up!" (Instead of "One large thick crust pizza with coming up!").
 Alter servePizza in a similar way to fix its grammar when serving a "plain" cheese pizza. */
 
 function getPizzaOrder(size,crust,...toppings) {
-  let pizzaArr =[size,crust,toppings];
-  if (pizzaArr.length == 3){
-   return console.log(`One ${pizzaArr[0], pizzaArr[1]} crust pizza with ${pizzaArr[2]} coming up!`), pizzaArr;
+  let pizzaArr =[size,crust,toppings]
+  if (pizzaArr.length = 3){
+   return console.log(`One ${pizzaArr[0], pizzaArr[1]} crust pizza with ${pizzaArr[2]} coming up!`), pizzaArr
   }
-  else if (pizzaArr.length == 2) {
-    return console.log(`One ${pizzaArr[0], pizzaArr[1]} crust pizza with cheese coming up!`), pizzaArr;
-  }
+  else if (pizzaArr.length < 3) {
+    return console.log(`One ${pizzaArr[0], pizzaArr[1]} crust pizza with cheese coming up!`), pizzaArr
+  };
 
   }
 getPizzaOrder ("small","thin","mushroom","tomato");
